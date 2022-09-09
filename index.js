224326c772296bea1413ddb6be5b41224302794a
MurituMaina <vincent.maina@student.moringaschool.com>
2022-09-09 06:59:21 +0300

    commit tree d19bba581ce4a0bed9e67610d6796483a3638064

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides
+    }
+  
+    get countSides() {
+      return this.sides.length
+    }
+  
+    get perimeter() {
+      let total = 0;
+      for (let side of this.sides) {
+        total += side
+      }
+      return total
+    }
+  }
+  
+  class Triangle extends Polygon {
+  
+    get isValid() {
+      let side0 = this.sides[0]
+      let side1 = this.sides[1]
+      let side2 = this.sides[2]
+      return ((side0 + side1 > side2) && (side0 + side2 > side1) && (side1 + side2> side0))
+    }
+  }
+  
+  class Square extends Polygon {
+  
+    get isValid() {
+
+      let side0 = this.sides[0]
+      let side1 = this.sides[1]
+      let side2 = this.sides[2]
+      let side3 = this.sides[3]
+      return ((side0 === side1) && (side0 === side2) && (side0 === side3))
+    }
+  
+    get area() {
+      return this.sides[0] * this.sides[0]
+    }
+  }
